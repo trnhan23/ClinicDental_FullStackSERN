8c496be75136f6dee456284313a148730eb742f9
Trong Nhan <cnttute21110571@gmail.com>
2024-09-10 13:26:32 +0700

    10/09/24-13:30

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,24 +1,46 @@
 import { Json } from 'sequelize/lib/utils';
 import db from '../models/index';
-
+import CRUDService from '../services/CRUDService';
 
 let getHomePage = async (req, res) => {
-    try{
+    try {
         let data = await db.User.findAll();
         //console.log(data)
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
         });
-    }catch(e){
+    } catch (e) {
         console.log(e);
     }
 }
 
-let getAboutPage = (rep, res) => {
+let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from server')
+}
+
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser();
+    console.log('---------------');
+    console.log(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    })
+}
+
 module.exports = {
-    getHomePage : getHomePage,
-    getAboutPage : getAboutPage,
+    getHomePage: getHomePage,
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
 }
---
+++ src/services/CRUDService.js
@@ -0,0 +1,57 @@
+import bcrypt from 'bcryptjs'
+import db from '../models/index';
+import { raw } from 'body-parser';
+const salt = bcrypt.genSaltSync(10);
+
+let createNewUser = async (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+            await db.User.create({
+                email: data.email,
+                password: hashPasswordFromBcrypt,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                address: data.address,
+                phonenumber: data.phonenumber,
+                gender: data.gender === '1' ? true : false,
+                roleId: data.roleId,
+            })
+            resolve('ok create a new user success');
+        } catch (e) {
+            reject(e);
+        }
+    })
+
+}
+
+let hashUserPassword = (pass) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPassword = await bcrypt.hashSync(pass, salt);
+            resolve(hashPassword);
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
+
+let getAllUser = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let users = db.User.findAll({
+                raw: true,
+            });
+            resolve(users);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+module.exports = {
+    createNewUser: createNewUser,
+    getAllUser: getAllUser,
+
+}
+

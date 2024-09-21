1bfb76c2cd75c1a70cf5f05445c6bce6033d14e4
Trong Nhan <cnttute21110571@gmail.com>
2024-09-21 10:00:53 +0700

    21/09/24-10:04

--- src/server.js
+++ src/server.js
@@ -2,15 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
-import connectDB from "./config/connectDB"
+import connectDB from "./config/connectDB";
+import cors from 'cors';
+// const cors = require("cors");
 require('dotenv').config();
 
-let app = express()
+let app = express();
+
+app.use(cors({ origin: true }));
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     res.setHeader('Access-Controls-Allow-Credentials', true);
+//     next();
+// });
 
 //config app
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 viewEngine(app);
 initWebRoutes(app);
